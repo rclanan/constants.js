db641bcddf46c14e81da34959a1b1875138138c9
Ray Clanan <rclanan@utopianconcept.com>
2015-01-29 11:52:18 -0500

    Add additional test for stringFormatter.js

--- test/spec/unit/stringFormatter.spec.js
+++ test/spec/unit/stringFormatter.spec.js
@@ -14,4 +14,12 @@ describe('String formatter Unit Test', function() {
   it('should return string with no replacements of value is not supplied', function() {
     expect(formatter.format('Hello {name}!', {})).toEqual('Hello {name}!');
   });
+  
+  it('should return same string when replacements are made and ignore additonal values', function() {
+    expect(formatter.format('Hello {name}!', { name: 'world', greeting: 'Hello' })).toEqual('Hello world!');
+  });
+
+  it('should return replaced values when called with multiple replacements', function() {
+    expect(formatter.format('{greeting} {name}!', { name: 'world', greeting: 'Hello' })).toEqual('Hello world!');
+  });
 });
