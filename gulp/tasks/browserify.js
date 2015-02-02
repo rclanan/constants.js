12c6810f5b20dfb65b55ca00f169247cabf60a16
Programmer Without a Name <Talk@programmerWithoutA.Name>
2015-02-02 16:13:01 -0500

    removed remapify

--- gulp/tasks/browserify.js
+++ gulp/tasks/browserify.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var browserify, remapify, browserifyTask, browserSync, watchify, bundleLogger, gulp, handleErrors, source, buffer, sourcemaps, config, wrap;
+var browserify, browserifyTask, browserSync, watchify, bundleLogger, gulp, handleErrors, source, buffer, sourcemaps, config, wrap;
 
 browserify = require('browserify');
 browserSync = require('browser-sync');
@@ -13,7 +13,6 @@ source = require('vinyl-source-stream');
 buffer = require('vinyl-buffer');
 config = require('../config').browserify;
 wrap = require('gulp-wrap');
-remapify = require('remapify');
 
 browserifyTask = function(callback, devMode) {
   var bundleQueue, browserifyThis;
@@ -21,7 +20,7 @@ browserifyTask = function(callback, devMode) {
   bundleQueue = config.bundleConfigs.length;
   browserifyThis = function(bundleConfig) {
     var browserifyLoader, bundle, reportFinished;
-    
+
     if (devMode) {
       _.extend(bundleConfig, watchify.args, { debug: true });
       bundleConfig = _.omit(bundleConfig, ['external', 'require']);
